35753f355c409bea90cac392f513adbb27d493af
Sanjay MS <sanjaysm@sulekha.com>
2023-03-29 17:54:08 +0530

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <div class="heading">
+            <h1 style="color: green; font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;">Project Management System</h1>
+        </div>
+        <div class="project-list">
+            <div class="project-list-child">
+                <a target="blank" href="https://expense-tracker-vuejs-by-sanjay.netlify.app/"><button
+                        style="background-color: yellowgreen;">Expense tracker</button></a>
+                <p class="margin-top">Click above to view</p>
+            </div>
+            <div class="project-list-child">
+                <a target="blank" href="https://quiz-app-by-sanjay.netlify.app/"><button style="background-color: yellowgreen;">Quiz
+                        app</button></a>
+                <p class="margin-top">Click above to view</p>
+            </div>
+            <div class="project-list-child">
+                <a target="blank" href="https://weather-app-by-sanjay.vercel.app/"><button style="background-color: yellowgreen;">Weather
+                        app</button></a>
+                <p class="margin-top">Click above to view</p>
+            </div>
+            <div class="project-list-child">
+                <a target="blank" href="https://covid-tracker-vue-sanjay.vercel.app/"><button
+                        style="background-color: yellowgreen;">COVID-Tracker</button></a>
+                <p class="margin-top">Click above to view</p>
+            </div>
+            <div class="project-list-child">
+                <a target="blank" href="https://calculator-vue-sanjay.netlify.app/"><button
+                        style="background-color: yellowgreen;">Calculator app</button></a>
+                <p class="margin-top">Click above to view</p>
+            </div>
+            <div class="project-list-child">
+                <a target="blank" href="https://todo-app-by-sanjay.netlify.app/"><button
+                        style="background-color: yellowgreen;">To-do-List</button></a>
+                <p class="margin-top">Click above to view</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
